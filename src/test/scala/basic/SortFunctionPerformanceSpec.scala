a83438c4c52ce7eb786648ba56f4f82aebc57138
KnewHow <948170910@qq.com>
2018-10-17 19:40:15 +0800

    cancel redundant log

--- src/test/scala/basic/SortFunctionPerformanceSpec.scala
+++ src/test/scala/basic/SortFunctionPerformanceSpec.scala
@@ -7,7 +7,7 @@ import prop.gen.Gen
 
 class SortFunctionPerformanceSpec extends FlatSpec {
   "merge-sort performance" should "better than insertion-sort" in {
-    val s  = Gen.run(Gen.listOfN(10, Gen.choose(1, 1000))).take(10).toList
+    val s  = Gen.run(Gen.listOfN(10000, Gen.choose(1, 1000))).take(10).toList
     val b1 = System.currentTimeMillis
     s.map { r =>
       Sorted.insertionSort(r)
@@ -17,8 +17,6 @@ class SortFunctionPerformanceSpec extends FlatSpec {
       Sorted.mergeSorted(r)
     }
     val sortE = System.currentTimeMillis
-    Logger.info(s"inserion-sort took->${mergeSortB - b1}")
-    Logger.info(s"merge-sort took->${sortE - mergeSortB}")
     assert(sortE - mergeSortB < mergeSortB - b1)
   }
 }
